358bdf6c4beb7cf57b402e32c9e3b30c0f8e7d04
Naved Ansari <naved001@gmail.com>
2019-08-19 15:19:39 -0400

    Add a new function to take of hosts that no longer exist in openstack
    This method will delete the hosts from zabbix after some amount of time.
    The time will be made configurable soon.
    
    Remove some print statements and unnecessary error handling.

--- zabbix_libvirt/main.py
+++ zabbix_libvirt/main.py
@@ -2,6 +2,7 @@
 
 import json
 import functools
+import time
 
 from pyzabbix import ZabbixMetric, ZabbixSender
 from pyzabbix.api import ZabbixAPIException
@@ -20,7 +21,6 @@ def update_instance(domain_uuid_string, libvirt_connection, zabbix_sender):
     `libvirt_connection` and then send the zabbix metrics using `zabbix_sender`
     """
     # 1. Discover nics and disks, and send the discovery packet
-    print("Updating instance")
     vnics = libvirt_connection.discover_vnics(domain_uuid_string)
     vdisks = libvirt_connection.discover_vdisks(domain_uuid_string)
 
@@ -68,19 +68,49 @@ def update_instance(domain_uuid_string, libvirt_connection, zabbix_sender):
                                     "libvirt.instance[{}]".format(stat), value))
 
     zabbix_sender.send(metrics)
-    print("Done updating")
+
+
+def cleanup_hosts(hosts, zabbix_api):
+    """
+    This function takes care of hosts that are in zabbix but no longer exist
+    in openstack.
+    If the host was not updated in a configurable amount of time, then we delete it.
+    """
+
+    # This can be any arbritary item that we know exists and is generally updated.
+    item_key = "libvirt.instance[name]"
+    retention_period = 90 * 24 * 60 * 60
+    hosts_to_be_deleted = []
+
+    for host in hosts:
+
+        host_id = zabbix_api.get_host_id(host)
+        assert host_id is not None, "Host ID is none for: " + host
+
+        lastclock = zabbix_api.get_item(host_id, item_key, "lastclock")
+
+        if lastclock is None:
+            logger.warning("Host '%s' has no lastclock", host)
+            continue
+
+        if int(time.time()) - int(lastclock) > retention_period:
+            logger.info("Staging host  '%s' to be deleted", host)
+            hosts_to_be_deleted.append(host_id)
+
+    if hosts_to_be_deleted != []:
+        zabbix_api.delete_hosts(hosts_to_be_deleted)
 
 
 def main():
     """main I guess"""
-
-    logger = setup_logging(__name__, LOG_FILE)
     host_list = get_hosts(HOSTS_FILE)
 
     custom_wrapper = functools.partial(
         PyZabbixPSKSocketWrapper, identity=PSK_IDENTITY, psk=bytes(bytearray.fromhex(PSK)))
     zabbix_sender = ZabbixSender(
         zabbix_server=ZABBIX_SERVER, socket_wrapper=custom_wrapper, timeout=30)
+
+    all_openstack_instances = []
 
     with ZabbixConnection(USER, "https://" + ZABBIX_SERVER, PASSWORD) as zapi:
 
@@ -94,26 +124,32 @@ def main():
             try:
                 libvirt_connection = LibvirtConnection(uri)
             except LibvirtConnectionError as error:
-                print(error)
+                # Log the failure to connect to a host, but continue processing
+                # other hosts.
                 logger.exception(error)
                 continue
-            except Exception as error:
-                print(error)
-                logger.exception(error)
-                raise
 
             domains = libvirt_connection.discover_domains()
+            all_openstack_instances.extend(domains)
             for domain in domains:
                 try:
                     if zapi.get_host_id(domain) is None:
                         zapi.create_host(
                             domain, groupid, templateid, PSK_IDENTITY, PSK)
                     update_instance(domain, libvirt_connection, zabbix_sender)
-                except ZabbixAPIException as error:
-                    print("************EXCEPTION************")
-                    print(error)
                 except DomainNotFoundError as error:
-                    print(error)
+                    # This may happen if a domain is deleted after we discover
+                    # it. In that case we log the error and move on.
+                    logger.exception(error)
+                except ZabbixAPIException as error:
+                    logger.exception(error)
+                    raise
+
+        all_zabbix_hosts = zapi.get_all_hosts()
+        hosts_not_in_openstack = list(
+            set(all_zabbix_hosts) - set(all_openstack_instances))
+
+        cleanup_hosts(hosts_not_in_openstack, zapi)
 
 
 if __name__ == "__main__":
@@ -128,4 +164,5 @@ if __name__ == "__main__":
     KEY_FILE = config['general']['KEY_FILE']
     GROUP_NAME = "openstack-instances"
     TEMPLATE_NAME = "moc_libvirt_single"
+    logger = setup_logging(__name__, LOG_FILE)
     main()
